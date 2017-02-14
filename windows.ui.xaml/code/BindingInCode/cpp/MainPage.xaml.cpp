fe4caf80f450a5276c96488518932b768357fee3
mhawker <michael.hawker@outlook.com>
2017-02-14 12:40:36 -0800

    Add BindingInCode C++/CX Example & Project.

---
+++ windows.ui.xaml/code/BindingInCode/cpp/MainPage.xaml.cpp
@@ -0,0 +1,40 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace BindingInCode;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	//<SnippetBindingObject>
+	// Create the source string.
+	String^ s = ref new String(L"Hello");
+
+	// Create the binding description.
+	Binding^ b = ref new Binding();
+	b->Mode = BindingMode::OneTime;
+	b->Source = s;
+
+	// Attach the binding to the target.
+	MyText->SetBinding(MyText->TextProperty, b);
+	//</SnippetBindingObject>
+}
